7a865547de059b403f00c20f86f270fbe3933743
Trevor Jennings <trevorjennings23@gmail.com>
2017-10-02 22:28:59 -0400

    added more logic

--- src/App.js
+++ src/App.js
@@ -57,43 +57,71 @@ class Scoreboard extends Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.clearScoreboard = this.clearScoreboard.bind(this);
   }
   handleSubmit(event) {
     event.preventDefault();
-    let newPlayersArray = this.state.players.slice();  
-    let newPlayer = { 
-      name: this.state.value, 
-      id: 3
-    };
-    newPlayersArray.push(newPlayer);   
-    this.setState({
-      players:newPlayersArray,
-      value: '',
-    });
+    if(this.state.value !== '') {
+      let newPlayersArray = this.state.players.slice();  
+      let newPlayer = { 
+        name: this.state.value, 
+        id: 3
+      };
+      newPlayersArray.push(newPlayer);   
+      this.setState({
+        players:newPlayersArray,
+        value: '',
+      });
+    } else {
+      document.getElementById('error').classList.add('flash-error');
+      setTimeout(function() {
+        document.getElementById('error').classList.remove('flash-error');
+      }, 1000);
+    }
   }
   handleChange(event) {
     this.setState({value: event.target.value});
   }
+  clearScoreboard() {
+    this.setState({
+      players: [],
+      value: '',
+    });
+  }
   render() {
     return (
-      <div className="wrapper">
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-        <div className="scoreboard">
-          <header className="header">
-            <h1>Game Scoreboard</h1>
-          </header>
-          {
-            this.state.players.map((player) => 
-              <Player name={player.name} score={player.score} key={player.id} />
-            )
-          }
-        </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-4">
+            <div>
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <label>
+                    Add Player:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control" />
+                  </label>
+                  <button type="submit" className="btn btn-default">submit</button>
+                </div>  
+              </form>
+              <div className="text-danger" id="error"><strong>Field is empty!</strong></div>
+            </div>
+            <div>
+              <button type="submit" className="btn btn-danger" onClick={this.clearScoreboard}>Clear Scoreboard</button>
+            </div>  
+          </div>
+          <div className="col-xs-12 col-md-8">  
+            <div className="scoreboard">
+              <header className="header">
+                <h1>Game Scoreboard</h1>
+              </header>
+              {
+                this.state.players.map((player) => 
+                  <Player name={player.name} score={player.score} key={player.id} />
+                )
+              }
+            </div>
+          </div>  
+        </div>  
       </div>  
     )
   }
@@ -103,7 +131,7 @@ class Scoreboard extends Component {
 class App extends Component {
   render() {
     return (
-     <Scoreboard />
+      <Scoreboard />
     );
   }
 }
